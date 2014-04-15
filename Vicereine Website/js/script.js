96b37bd1435de4a56b553a7410efaeaa02d83002
Erich Johan Uher <ejuher@gmail.com>
2014-04-14 21:07:29 -0700

    Initial commit. Commented as a test of Git.

---
+++ Vicereine Website/js/script.js
@@ -0,0 +1,123 @@
+/*
+$("div div").hover(function() {
+	var row = $(this).parent().index();
+	var col = $(this).index();
+	$(this).css("background-color", "black"); 
+	console.log(row); 
+	console.log(col);
+	console.log('content above: ' + $(this).parent().parent().innerHTML)
+}, 
+
+	function() {$(this).css("background-color", "#26EDED"); 
+	console.log(this.innerHTML)
+});
+
+
+//change the content of a cell given row 2, col 11
+$('.container:nth-of-type(2) :nth-child(11)').html('V')
+
+*/
+
+$("div div").hover(function() {
+	if ($(this).html() != '') {
+		//index function returns zero-based position
+		MoveText($(this).parent().index(), $(this).index());
+	}
+})
+
+//note: .index returns values starting at 0, BUT nth-child and nth-of type accept
+//values starting at 1. That means for a row = box.parent().index(), to access the
+//the row above use .container:nth-of-type(row)
+
+function GetRandom(min,max) {
+	var r = Math.floor(Math.random() * (max - min +1)) + min;
+	return r;
+}
+
+function MoveText(row,col) {
+	var moves = [true, true, true, true]; //directions available for letter to move to (up, right, down, left)
+	var thisbox = '.container:nth-of-type('+(row+1)+') :nth-child('+(col+1)+')';
+	var abovebox = '.container:nth-of-type('+(row)+') :nth-child('+(col+1)+')';
+	var rightbox = '.container:nth-of-type('+(row+1)+') :nth-child('+(col+2)+')';
+	var belowbox = '.container:nth-of-type('+(row+2)+') :nth-child('+(col+1)+')';
+	var leftbox = '.container:nth-of-type('+(row+1)+') :nth-child('+(col)+')';
+
+	//for testing 
+	console.log(row);
+	console.log(col);
+
+	//if you are an edge box then invalidate moving/checking outside the box
+	if (row === 0) { //if in top row
+		moves[0] = false;
+	}
+	if (row == 2) { //if in bottom row
+		moves[2] = false;
+	}
+	if (col == 0) { //if on left edge
+		moves[3] = false;
+	}	
+	if (col == 10) { //if on right edge
+		moves[1] = false;
+	}
+
+	//check if adjacent boxes are occupied
+	if (moves[0] === true) { //if box is not in row 1, then if box above is empty
+		if ($(abovebox).html() != '') {
+			moves[0] = false;
+		}
+	}
+	if (moves[1] === true) { //if box is not on right edge, then if box to the right is empty
+		if ($(rightbox).html() != '') {
+			moves[1] = false;
+		}
+	}
+	if (moves[2] === true) { //if box is not in row 3, then if box below is empty
+		if ($(belowbox).html() != '') {
+			moves[2] = false;
+		}
+	}
+	if (moves[3] === true) { //if box is not on left edge, then if box to the left is empty
+		if ($(leftbox).html() != '') {
+			moves[3] = false;
+		}
+	}
+
+	//check if number has any possible moves or if it is trapped
+	if (!(moves[0]===false && moves[1]===false && moves[2]===false && moves[3]===false)) { 
+
+		//generate random number 0-3
+		var max = 3;
+		var min = 0;
+
+		var rand = GetRandom(min,max);
+
+		//if a given move is unavailable, then try again
+		while (moves[rand]===false) {
+			rand = GetRandom(min,max);
+		}
+
+		console.log('rand: ' + rand);
+		console.log('thisbox: ' + thisbox);
+
+		//move the letter to the random box
+		if (rand===0) { //move the letter up
+			$(abovebox).html($(thisbox).html());
+			$(thisbox).html('');
+		} 
+		else if (rand===1) {
+			$(rightbox).html($(thisbox).html());
+			$(thisbox).html('');
+		}
+		else if (rand===2) {
+			$(belowbox).html($(thisbox).html());
+			$(thisbox).html('');
+		}
+		else if (rand===3) {
+			$(leftbox).html($(thisbox).html());
+			$(thisbox).html('');
+		}
+
+	}
+}
+
+
---
+++ chelsea bday/script.js
@@ -0,0 +1,25 @@
+//when you click yes
+$("#yes1").click(function() {
+	$(".q2").show(1500);
+})
+  //unhide q2
+
+//when you click no
+$("#no1").click(function() {
+	alert('Get outta here!');
+})
+  //alert: get outta here!
+
+//when you click yes on q2
+$("#yes2").click(function() {
+	while (1 != 2) {
+		alert('HAPPY BIRTHDAY')
+	}
+})
+  //change background? to HAPPY BIRTHDAY CHELSEA!
+
+//when you click no
+$("#no2").click(function() {
+	alert('OK! Have a nice day Chelsea :)')
+})
+  //alert: OK, have a nice day Chelsea :)
